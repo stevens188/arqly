9dcba671a8e277269ba5c63190b2ae1e79016657
stevens sanchez <stevenssanchezdelgadillo@gmail.com>
2022-08-25 04:04:04 -0500

    dejando ingles igual a español

--- lang/en.php
+++ lang/en.php
@@ -1,10 +1,10 @@
 <?php 
 function lang($val=null) {
   $lang=[
-    'html_title' => 'Revit drafting service',
-    'quienes_somos' => 'about Us',
-    'contact' => 'contact',
-    'modelado_3d_description' => 'Aqui pongo el carretazo'
+    'html_title' => 'Servicios de dibujo en Revit',
+    'quienes_somos' => '¿Quiénes somos?',
+    'contact' => 'contacto.',
+    'modelado_3d_description' => 'Te ayudamos con los planos de tu proyecto, modelado 3d y creación de planos. Así mismo, con cantidades de obra y, si lo requiere, podemos ayudarle con el diseño. Te enviaremos los planos en CAD, RVT y PDF.',
   ];
   if(isset($lang[$val])) {
     return $lang[$val];
